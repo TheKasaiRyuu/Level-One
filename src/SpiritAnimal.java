c9e95bf778d3450c4b56817fd04e4dcdb8ac0ef1
Savera <Level 2>
2016-06-30 16:38:56 -0700

    Classes and Methods
    Making Classes and Methods

---
+++ src/SpiritAnimal.java
@@ -0,0 +1,12 @@
+
+public class SpiritAnimal {
+	public static void main(String[] args) {
+	Lion Leo = new Lion ("Leo", "yellow", 6, 15);
+	Lion Scar = new Lion ("Scar", "brown", 0, 30);
+	Leo.intro();	
+	Leo.loyalty();
+	Scar.intro();
+	Scar.loyalty();
+	}
+
+}
