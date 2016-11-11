3e362a36631b71de41d97ed8c2fd36b3905a2c70
TheKasaiRyuu <league@Mac-mini-3.local>
2016-11-10 16:56:25 -0800

    babies head is off
    bnaisfgosiduzoauhetdflhwsgdalhsbajvalknbdsfanmds
    bqglebaldsjgfavqvkjdoausvqljnbuyghpoop

---
+++ src/LandOfTurtles.java
@@ -0,0 +1,36 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import org.teachingextensions.logo.MultiTurtlePanel;
+import org.teachingextensions.logo.Turtle;
+
+
+public class LandOfTurtles {
+	public static void main(String[] args) {
+		MultiTurtlePanel panel = new MultiTurtlePanel();
+		String galapagosIslands = "https://farm2.staticflickr.com/1104/752631367_5c5d474ba5_o.jpg";
+
+
+// 1. Create a frame & make it visible
+ JFrame frame = new JFrame();
+ JPanel Panel = new JPanel();
+frame.setVisible(true);
+frame.add(Panel);
+
+		// 2. Add the panel to the frame
+		
+		// 3. Set the background image of the panel to the Galapagos Islands 
+
+
+		// 4. Instantiate a Turtle 
+
+
+// 5. Add the turtle to the panel 
+		
+		// 6. Put 50 Turtles on the beach
+
+
+	}
+}
+
+
--- src/Testlol.java
+++ src/Testlol.java
@@ -1,6 +1,7 @@
 
 public class Testlol {
 public static void main(String[] args) {
+
 	Beach Cancoon = new Beach("Cancoon", true, false, 9999);
 	Beach Backyard = new Beach("Backyard", false, true, 10);
 	Cancoon.bolleyBall();
@@ -9,6 +10,20 @@ public static void main(String[] args) {
 	}
 	Backyard.Nudes();
 	Backyard.sandCastle();
+	
+	SeaCreatures SpongeBob = new SeaCreatures("SpongeBob");
+	SpongeBob.eat();
+	SpongeBob.laugh();
+	SeaCreatures Patrik = new SeaCreatures("Patrick");
+	SeaCreatures Squidward = new SeaCreatures("Squidward");
+	String name = Patrik.getName();
+	System.out.println(name);
+	Patrik.eat();
+	Patrik.laugh();
+	String names = Squidward.getName();
+	System.out.println(names);
+	Squidward.eat();
+	Squidward.laugh();
 	
 }
 }
